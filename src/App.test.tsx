115269e72cd1b402e2f18d48d1e266b52aa9c761
rodolphe37 <rodolphe.a@gmail.com>
2023-06-18 10:24:37 +0200

    updated README file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-const versions = window.versions;
 const chrome = () => "114.0.5735.106";
 const node = () => "18.15.0";
 const electron = () => "25.1.1";
@@ -9,29 +8,13 @@ const ping = () => "pong";
 
 describe("Link's are present in screen", () => {
   test("renders react link", () => {
-    render(
-      <App
-        ping={ping}
-        chrome={chrome}
-        node={node}
-        electron={electron}
-        versions={versions}
-      />
-    );
+    render(<App ping={ping} chrome={chrome} node={node} electron={electron} />);
     const reactLink = screen.getByText("React");
     expect(reactLink).toBeInTheDocument();
   });
 
   test("renders electron link", () => {
-    render(
-      <App
-        ping={ping}
-        chrome={chrome}
-        node={node}
-        electron={electron}
-        versions={versions}
-      />
-    );
+    render(<App ping={ping} chrome={chrome} node={node} electron={electron} />);
     const electronLink = screen.getByText("Electron");
     expect(electronLink).toBeInTheDocument();
   });
